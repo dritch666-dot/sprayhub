3f20e257b0e6ea6a046e30087c793f93896fe77c
dritch666-dot <dritch666@gmail.com>
2026-04-23 06:49:26 +1000

    Fall Armyworm (FAW) Management Guide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sprayhub-v211';
+const CACHE_NAME = 'sprayhub-v212';
 
 const ASSETS = [
   './',
@@ -6,6 +6,7 @@ const ASSETS = [
   './lawn-turf-guide.html',
   './termite-control-guide.html',
   './house-fly-control-guide.html',
+  './fall-armyworm-management-guide.html',
   './weed-resistance-guide.html',
   './fruit-fly-control-guide.html',
   './mosquito-control-guide.html',
