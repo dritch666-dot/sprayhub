e3c45e34ee15e328a720d5578d70da16806a9c29
dritch666-dot <dritch666@gmail.com>
2026-04-23 07:46:19 +1000

    Vegetable Garden Pest & Disease Guide (Home + Small-Plot)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sprayhub-v212';
+const CACHE_NAME = 'sprayhub-v213';
 
 const ASSETS = [
   './',
@@ -6,6 +6,7 @@ const ASSETS = [
   './lawn-turf-guide.html',
   './termite-control-guide.html',
   './house-fly-control-guide.html',
+  './vegetable-garden-pest-disease-guide.html',
   './fall-armyworm-management-guide.html',
   './weed-resistance-guide.html',
   './fruit-fly-control-guide.html',
